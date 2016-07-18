935b29a2cf76ea41e6c313625382758bc85ae3d1
Taylor Miller <trmiller10@gmail.com>
2016-07-18 15:57:32 -0400

    commented out test

--- src/test/java/com/theironyard/TiyCrudSpringApplicationTests.java
+++ src/test/java/com/theironyard/TiyCrudSpringApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+/*
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TiyCrudSpringApplication.class)
@@ -12,20 +13,29 @@ import org.springframework.test.context.web.WebAppConfiguration;
 public class TiyCrudSpringApplicationTests {
 
     //find out what this could do
+*/
 /*	@Test
 	public void contextLoads() {
-	}*/
+	}*//*
 
-    /**
+
+    */
+/**
      * This test would assert that "/" GET route creates an HTML page
-     */
+     *//*
 
-    /**
+
+    */
+/**
      * This test would assert that the PSQL database is properly connected to
      * the program.
-     */
+     *//*
 
-    /**
+
+    */
+/**
      * This test would create a test user and assert that the user was entered into the PSQL database
-     */
+     *//*
+
 }
+*/
